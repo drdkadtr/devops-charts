ebfce055e79e114b5883e340c530bccc8de471d8
Admir Trakic <adtr@dr.dk>
2022-02-11 14:43:22 +0100

    Update ci.yaml

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 set -x
 
-git clone --depth=1 --single-branch  https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/"${GITHUB_REPO}".git
+git clone --depth=1 --single-branch https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/"${GITHUB_REPO}".git
 
 git add files index.yaml
 git commit -m "Update"
