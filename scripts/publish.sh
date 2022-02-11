b92f01f7a8e6d11f1a46fdd51b1e7ef1c1f0ef37
Admir Trakic <adtr@dr.dk>
2022-02-11 16:09:45 +0100

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,12 @@ set -eou pipefail
 set -x
 
 git config -l | cat -
-git clone --depth=1 --single-branch https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/"${GITHUB_REPO}".git
+git clone --depth=1 --single-branch https://"${PERSONAL_TOKEN}"@github.com/"${GITHUB_REPO}".git
+cp -af files index.yaml "${GITHUB_REPO}"/
 
+cd "${GITHUB_REPO}"
 git add files index.yaml
 git status -s . | cat -
-
+git remote -v
 git commit -m "Github Actions: $(date +"%Y-%m-%d %H:%M")"
-git push https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/"${GITHUB_REPO}".git
+git push -v https://"${PERSONAL_TOKEN}"@github.com/"${GITHUB_REPO}".git
