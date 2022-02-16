4c7ee6c32efaeac2f332582c249aaa22f51b3411
Admir Trakic <adtr@dr.dk>
2022-02-16 13:44:25 +0100

    Fix kubeval

--- scripts/kubeval.sh
+++ scripts/kubeval.sh
@@ -8,7 +8,7 @@ KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 if ! [ -x "$(command -v kubeval)" ]; then
-  curl curl -sSL -o /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
+  curl -sSL -o /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
   sudo tar -C /usr/local/bin -xf /tmp/kubeval.tar.gz kubeval
 fi
 
