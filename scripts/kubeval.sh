0a4ea36cf17c3257c6618be4ab3956c0c066d7d8
Admir Trakic <adtr@dr.dk>
2022-02-16 13:46:46 +0100

    Fix kubeval

--- scripts/kubeval.sh
+++ scripts/kubeval.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -e
-set -u
-set -x
-
 KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
@@ -11,6 +7,10 @@ if ! [ -x "$(command -v kubeval)" ]; then
   curl -sSL -o /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
   sudo tar -C /usr/local/bin -xf /tmp/kubeval.tar.gz kubeval
 fi
+
+set -e
+set -u
+set -x
 
 for dir in charts/*; do
     echo "Helm dependency build..."
