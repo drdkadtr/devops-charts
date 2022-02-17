ace237162779028f26e4cd8fb54fdc5d5710c1b9
Admir Trakic <adtr@dr.dk>
2022-02-17 11:13:20 +0100

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,14 +4,13 @@ set -eou pipefail
 set -x
 
 git clone -b "$BRANCH" --depth=1 --single-branch https://"${PERSONAL_TOKEN}"@github.com/"${GITHUB_REPO}".git "${GITHUB_REPO}"
+mkdir -p "${GITHUB_REPO}"/files/
 cp -af files/* "${GITHUB_REPO}"/files/
 
-cd "${GITHUB_REPO}" || exit 1
 helm repo index .
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
 git add files index.yaml
 git status -s . | cat -
 git remote -v
